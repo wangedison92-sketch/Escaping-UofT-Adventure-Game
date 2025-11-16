a9510e05d3938b4a880ef613006eea681ca9b0ef
Kimberly Prijadi <prijadikimberly@gmail.com>
2025-11-16 18:44:33 -0500

    updated files following the code in ca-lab

--- src/main/java/interface_adapter/ViewManagerModel.java
+++ src/main/java/interface_adapter/ViewManagerModel.java
@@ -1,5 +1,14 @@
-- models
-- presenters
-- states???
+package main.java.interface_adapter;
 
-- separate per use case i think
+/**
+ * Model for the View Manager. Its state is the name of the View which
+ * is currently active. An initial state of "" is used.
+ */
+public class ViewManagerModel extends ViewModel<String> {
+
+    public ViewManagerModel() {
+        super("view manager");
+        this.setState("");
+    }
+
+}
--- src/main/java/view/ViewManager.java
+++ src/main/java/view/ViewManager.java
@@ -1,6 +1,6 @@
-package view;
+package main.java.view;
 
-import interface_adapter.ViewManagerModel;
+import main.java.interface_adapter.ViewManagerModel;
 
 import javax.swing.*;
 import java.awt.*;
