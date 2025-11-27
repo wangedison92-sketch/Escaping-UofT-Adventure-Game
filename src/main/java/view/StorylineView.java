245473d668ac9f1f2e97230b927b77a2fa1960dc
vhanbao2006 <vanessa.hanbao@mail.utoronto.ca>
2025-11-26 19:00:32 -0500

    Added some simple UI to trivia game

--- src/main/java/view/StorylineView.java
+++ src/main/java/view/StorylineView.java
@@ -2,6 +2,7 @@ package view;
 
 public class StorylineView {
     public StorylineView(String imageFilePath, String storylineText, String nextViewName) {
+        // idea: create a generic storyline template that can be reused for different locations
     }
 }
 
