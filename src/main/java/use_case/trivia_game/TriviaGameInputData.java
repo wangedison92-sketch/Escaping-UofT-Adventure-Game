460d3e6b9482b92226881e639466b0ec3dae68c8
Gabriel <75056711+gkgeo9@users.noreply.github.com>
2025-11-29 22:26:24 -0500

    refactoring my trivia code

--- src/main/java/use_case/trivia_game/TriviaGameInputData.java
+++ src/main/java/use_case/trivia_game/TriviaGameInputData.java
@@ -1,14 +1,30 @@
 package use_case.trivia_game;
 
 public class TriviaGameInputData {
+    public enum Action {
+        START_NEW_QUESTION,
+        SUBMIT_ANSWER,
+        EXIT_PUZZLE
+    }
+
+    private final Action action;
     private final String playerAnswer;
 
-    public TriviaGameInputData(String playerAnswer) {
+    public TriviaGameInputData(Action action) {
+        this.action = action;
+        this.playerAnswer = null;
+    }
+
+    public TriviaGameInputData(Action action, String playerAnswer) {
+        this.action = action;
         this.playerAnswer = playerAnswer;
+    }
+
+    public Action getAction() {
+        return action;
     }
 
     public String getPlayerAnswer() {
         return playerAnswer;
     }
-
 }
